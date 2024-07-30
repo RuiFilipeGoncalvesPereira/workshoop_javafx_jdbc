b041a7a70482b24d820be37395246ab3aa3bc6c2
Rui Pereira <rui6.319@hotmail.com>
2024-07-30 11:34:19 +0100

    Adding database access

--- src/module-info.java
+++ src/module-info.java
@@ -3,6 +3,7 @@ module Work_javafx_jdbc {
 	requires javafx.fxml;
 	requires javafx.graphics;
 	requires javafx.base;
+	requires java.sql;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.graphics,  javafx.fxml;
